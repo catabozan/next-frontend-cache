0cfab1c1dde16d4285add158043bb64d03508fdb
Catalin Bozan <catabozan@protonmail.com>
2022-05-04 16:03:12 +0300

    config: change cacheing period to 30 days

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
             headers: [
                 {
                     key: 'Cache-Control',
-                    value: 'public,max-age:31536000'
+                    value: 'public,max-age:2592000'
                 }
             ]
         }
